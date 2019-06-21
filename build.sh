7bc8c0a56ad6335ed1c0b6bab53c36b0565950f4
Michael Lihs (CI/OSC1) <michael.lihs@de.bosch.com>
2019-06-21 12:17:49 +0200

    improve build script for resource image

--- build.sh
+++ build.sh
@@ -1,3 +1,25 @@
-docker build --no-cache -t rocket-notify-resource .
-docker tag rocket-notify-resource kkmbal/rocket-notify-resource
-docker push kkmbal/rocket-notify-resource
+#!/usr/bin/env bash
+
+#/ build.sh [VERSION] [REPOSITORY]
+#/
+#/ Builds the image for the Concourse resource, tags it with the provided VERSION
+#/ (or :latest if no version is given) and pushes it to
+#/ a given repository on Dockerhub or any DTR (or using 'michaellihs' as default repo).
+#/
+#/ Examples:
+#/
+#/ ./build.sh latest michaellihs
+#/
+#/ Options:
+#/   --help: Display this help message
+usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
+expr "$*" : ".*--help" > /dev/null && usage
+
+version=${1:-latest}
+repo=${2:-michaellihs}
+
+image_name='rocket-notify-resource'
+
+docker build --no-cache -t ${image_name} .
+docker tag ${image_name} ${repo}/${image_name}:${version}
+docker push ${repo}/${image_name}:${version}
