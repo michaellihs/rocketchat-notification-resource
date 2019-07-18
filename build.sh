4bbd69161f071a49601156e3935450b341939f0a
Michael Lihs (CI/OSC1) <michael.lihs@de.bosch.com>
2019-07-18 16:54:17 +0200

    improve build script

--- build.sh
+++ build.sh
@@ -4,21 +4,22 @@ set -e
 
 #/ build.sh [VERSION] [REPOSITORY]
 #/
-#/ Builds the image for the Concourse resource, tags it with the provided VERSION
-#/ (or :latest if no version is given) and pushes it to
-#/ a given repository on Dockerhub or any DTR (or using 'michaellihs' as default repo).
+#/ Builds the image for the Concourse resource, tags it with the provided VERSION and pushes it to
+#/ a given repository on Dockerhub or any DTR.
 #/
 #/ Examples:
 #/
-#/ ./build.sh latest michaellihs
+#/ ./build.sh VERSION REPOSITORY
 #/
 #/ Options:
 #/   --help: Display this help message
 usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
 expr "$*" : ".*--help" > /dev/null && usage
 
-version=${1:-latest}
-repo=${2:-michaellihs}
+if [[ -z $1 || -z $2 ]]; then usage ; fi
+
+version=$1
+repo=$2
 
 image_name='rocket-notify-resource'
 
